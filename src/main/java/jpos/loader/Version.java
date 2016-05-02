517be9201a91a7a28267515f918b1a3d4ec13912
Denis Kuniß <kuniss@grammarcraft.de>
2016-05-02 23:43:46 +0200

    read version from manifest file in the jar
    to get consistent version output

--- src/main/java/jpos/loader/Version.java
+++ src/main/java/jpos/loader/Version.java
@@ -1,5 +1,11 @@
 package jpos.loader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 ///////////////////////////////////////////////////////////////////////////////
 //
 // This software is provided "AS IS".  The JavaPOS working group (including
@@ -46,5 +52,54 @@ public final class Version extends Object
     // Class variables
     //
 
-    private static final String JCL_VERSION_STRING = "2.3.0-RC3";
+    private static final String JCL_VERSION_STRING = getImplementationVersion();
+    
+    final private static String getImplementationVersion()
+    {
+        try
+        {
+            // Retrieve the corresponding manifest file of the calling class
+            String classContainer = Version.class.getProtectionDomain().getCodeSource().getLocation().toString();
+            
+            Manifest manifest = readManifest(classContainer);
+
+            // Retrieve all the custom entries included in the manifest file
+            Attributes.Name keyName = new Attributes.Name("Implementation-Version");
+            Attributes mainAttributes = manifest.getMainAttributes();
+            if (mainAttributes == null || !mainAttributes.containsKey(keyName)) {
+                return "version_determination_failed";
+            }
+            
+            return mainAttributes.getValue(keyName);
+        }
+        catch (Exception e)
+        {
+            return "version_determination_failed";
+        }
+    }
+
+	private static Manifest readManifest(String classContainer) throws IOException {
+        URL manifestUrl = new URL("jar:" + classContainer + "!/META-INF/MANIFEST.MF");
+        InputStream manifestIn;
+    	try {
+    		manifestIn = manifestUrl.openStream();
+    	} 
+    	catch (IOException ioe) {
+    		// try from file for testing and development environments
+    		try  {
+    			manifestIn = new URL(classContainer + "/META-INF/MANIFEST.MF").openStream();
+        	} 
+    		catch (IOException ioeForFileAccess) {
+        		throw ioe;
+        	}
+    	}
+        	
+        try {
+        	return new Manifest(manifestIn);
+        } 
+        finally {
+        	try { manifestIn.close(); }	catch (IOException ioe) {} // ignore exception 
+        }
+	}
+
 }
